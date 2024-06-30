34801cd9cd526fa309c3ce46f7afa278737ba536
Roberto Salas <robertosalas@Robertos-iMac.local>
2024-06-29 20:46:52 -0600

    diff and diagnostics
    Removed setup for diagnostics & diff on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,11 +4,8 @@ return {
         require('lualine').setup({
             sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff' },
-                lualine_c = {
-                    'diagnostics',
-                    sources = { 'nvim_lsp' },
-                },
+                lualine_b = { 'branch' },
+                lualine_c = { 'filename' },
                 lualine_x = { 'filetype', 'filesize', 'progress' },
                 lualine_y = { 'encoding' },
                 lualine_z = { 'datetime' },
