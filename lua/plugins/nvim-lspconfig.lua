8b14f8bd13bd1ab45098d65f0600df0b9817ca25
Roberto Salas <robertosalas@Robertos-iMac.local>
2024-07-15 21:29:50 -0600

    Fix nvim-cmp update breaking change
    nvim-cmp last update broke Tab snippet item selection.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -51,11 +51,17 @@ return {
         require('runner').setup({})
 
         -- CMP & LuaSnip
+        require('luasnip').setup({})
+
         cmp.setup({
             snippet = {
                 expand = function(args)
                     luasnip.lsp_expand(args.body)
                 end
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
@@ -68,8 +74,8 @@ return {
                 ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
+                    elseif luasnip.locally_jumpable() then
+                        luasnip.jump(1)
                     else
                         fallback()
                     end
@@ -77,7 +83,7 @@ return {
                 ['<S-Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
+                    elseif luasnip.locallly_jumpable(-1) then
                         luasnip.jump(-1)
                     else
                         fallback()
